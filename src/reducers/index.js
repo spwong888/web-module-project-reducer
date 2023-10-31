ea96681038ba59e13a05a4de7165e88d08ac17a6
Spencer Wong <sawong887@gmail.com>
2023-10-30 23:29:07 -0700

    newer version

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { 
+    ADD_ONE, 
+    APPLY_NUMBER, 
+    CHANGE_OPERATION, 
+    CLEAR_DISPLAY, 
+    MEMORY_ADD, 
+    MEMORY_APPLY, 
+    MEMORY_CLEAR
+} from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -14,32 +22,51 @@ const calculateResult = (num1, num2, operation) => {
             return num1 * num2;
         case("-"):
             return num1 - num2;
+        default:
+            return num1;
     }
 }
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case(ADD_ONE):
-            return({
+    switch (action.type) {
+        case ADD_ONE:
+            return {
                 ...state,
                 total: state.total + 1
-            });
-
-        case(APPLY_NUMBER):
-            return ({ 
-                ...state, 
+            };
+        case APPLY_NUMBER:
+            return {
+                ...state,
                 total: calculateResult(state.total, action.payload, state.operation)
-            });
-        
-        case(CHANGE_OPERATION):
-            return ({
+            };
+        case CHANGE_OPERATION:
+            return {
                 ...state,
                 operation: action.payload
-            });
-            
+            };
+        case CLEAR_DISPLAY:
+            return {
+                ...state,
+                total: 0
+            };
+        case MEMORY_ADD:
+            return {
+                ...state,
+                memory: state.total
+            };
+        case MEMORY_APPLY:
+            return {
+                ...state,
+                total: calculateResult(state.total, state.memory, state.operation)
+            };
+        case MEMORY_CLEAR:
+            return {
+                ...state,
+                memory: 0
+            };
         default:
             return state;
     }
-}
+};
 
 export default reducer;
